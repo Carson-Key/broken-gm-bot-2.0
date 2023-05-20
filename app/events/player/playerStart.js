a48ef4d72056f6835cafe6093327cf414aa6e5f1
Carson-Key <keyc@oregonstate.edu>
2023-05-20 13:04:43 -0700

    Add: playerStart event message

---
+++ app/events/player/playerStart.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "playerStart",
+	async execute(client) {
+		const queue = client.player.nodes.get(client.guild.id)
+		const title = queue.currentTrack.title
+		const interaction = queue.metadata
+		const channel = await client.guild.channels.fetch(interaction.channelId)
+
+		channel.send({content: `**Playing:** ${title}`})
+	},
+};
--- app/events/ready.js
+++ app/events/ready.js
@@ -1,3 +1,5 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { Events } = require('discord.js');
 const { Player } = require('discord-player');
 
@@ -9,6 +11,15 @@ module.exports = {
 		await player.extractors.loadDefault();
 
 		client.player = player;
+
+		const playerEventsPath = path.join(__dirname, 'player');
+		const playerEventFiles = fs.readdirSync(playerEventsPath).filter(file => file.endsWith('.js'));
+
+		for (const file of playerEventFiles) {
+			const filePath = path.join(playerEventsPath, file);
+			const event = require(filePath);
+			client.player.events.on(event.name, (...args) => event.execute(...args));
+		}
 
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
