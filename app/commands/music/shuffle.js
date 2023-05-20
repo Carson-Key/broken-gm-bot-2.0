0fff081e15bd2cc2feac81e14abfb475f4e446d4
Carson-Key <keyc@oregonstate.edu>
2023-05-20 12:10:26 -0700

    Add: Shuffle command

---
+++ app/commands/music/shuffle.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('shuffle')
+		.setDescription('Shuffle the queue'),
+	async execute(interaction) {
+        const queue = interaction.client.player.nodes.get(interaction.guild.id)
+
+        if (!queue) {
+            await interaction.reply('There is no queue');
+        }
+        
+        queue.tracks.shuffle();
+    
+        await interaction.reply('The queue has been shuffled');
+	},
+};
