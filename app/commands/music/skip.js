d14a710696a25794d9bf18e4523adc5fc99511cd
Carson-Key <keyc@oregonstate.edu>
2023-05-20 11:23:33 -0700

    Add: Skip command

---
+++ app/commands/music/skip.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('skip')
+		.setDescription('Skip currently playing'),
+	async execute(interaction) {
+        const queue = interaction.client.player.nodes.get(interaction.guild.id)
+
+        if (!queue) {
+            await interaction.reply('There nothing urrently playing');
+        }
+        
+        queue.node.skip();
+    
+        await interaction.reply('Skipped');
+	},
+};
