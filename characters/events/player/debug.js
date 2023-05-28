43d46db6d1e6096adf8ec10a0821d53d51bfe62e
Carson-Key <keyc@oregonstate.edu>
2023-05-28 11:55:44 -0700

    Move: mp3s folder to project root

--- characters/events/player/debug.js
+++ characters/events/player/debug.js
@@ -1,16 +1,13 @@
-import { fileURLToPath } from 'url';
 import fs from 'node:fs';
 import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import appRoot from 'app-root-path';
 
 export default {
 	name: "debug",
 	async execute(queue, message) {
 		if (message.includes("was marked as finished")) {
 			const matchingArray = /Track {"title":"(.*?)","isTransitionMode":false} was marked as finished/g.exec(message);
-			fs.unlink(path.join(__dirname, '..', '..', 'commands', 'aiVoices', 'mp3s', matchingArray[1]), (err) => {
+			fs.unlink(path.join(appRoot.path, 'mp3s', matchingArray[1]), (err) => {
 				if (err) {
 					console.error(err)
 					return
